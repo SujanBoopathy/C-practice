8b641a018a2b9e3770532a081e52e1cd32906015
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-11-16 15:35:46 +0530

    Update identicalNumbers.cpp

--- identicalNumbers.cpp
+++ identicalNumbers.cpp
@@ -9,7 +9,7 @@ bool checkSimilar(int arr[],int n){
     return false;
 }
 
-int main()
+void main()
 {
     int n;
     cout<<"Enter size :"<<endl;cin>>n;
@@ -18,6 +18,6 @@ int main()
     for(int i=0;i<n;i++)
         cin>>numsArray[i];   
     checkSimilar(numsArray,n)?cout<<"true":cout<<"false";
-    return 0;
+    
 }
 
