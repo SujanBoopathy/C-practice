cb4eda6ecbdc8e7349fca6f66705022e411ef652
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-12-14 23:33:54 +0530

    Update allZeroAtEnd.cpp

--- allZeroAtEnd.cpp
+++ allZeroAtEnd.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-void zeroAtEnd(int a[],int n){
+void zeroAtEnd(int a[],int size){
    int count=0;
-   for(int i=0;i<n;i++){
+   for(int i=0;i<size;i++){
        if(a[i]!=0)
           a[count++]=a[i];
    }
-   for(;count<n;count++){
+   for(;count<size;count++){
        a[count]=0;
    }
 }
@@ -21,7 +21,8 @@ void print(int a[],int n){
 
 int main(){
     int size;
-    cout<<"Enter size of the array :"<<endl;cin>>size;
+    cout<<"Enter size of the array :"<<endl;
+    cin>>size;
     int arr[size];
     cout<<"Enter the element of the array :"<<endl;
     for(int i=0;i<size;i++)   cin>>arr[i];
