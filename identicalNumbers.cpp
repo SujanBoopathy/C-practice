685b5869ebfa51c2c325158cb8900c61b8a66f28
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-12-23 20:49:45 +0530

    problem-solving

--- identicalNumbers.cpp
+++ identicalNumbers.cpp
@@ -9,15 +9,16 @@ bool checkSimilar(int arr[],int n){
     return false;
 }
 
-void main()
+int main()
 {
     int n;
     cout<<"Enter size :"<<endl;cin>>n;
-    int *numsArray=new int(n);
+    //int *numsArray=new int(n);
+    int numsArray[n];
     cout<<"Enter "<<n<<" elements :"<<endl; 
     for(int i=0;i<n;i++)
         cin>>numsArray[i];   
     checkSimilar(numsArray,n)?cout<<"true":cout<<"false";
-    
+    return 0;
 }
 
